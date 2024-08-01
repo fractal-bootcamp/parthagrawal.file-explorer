05dd990b8f45abf7e0690efc3f5cf088bd936b6e
Parth Agrawal <pvagrawal0@gmail.com>
2024-08-01 19:02:32 -0400

    refactor into factory function

--- backend/lib/services/s3Service.ts
+++ backend/lib/services/s3Service.ts
@@ -3,15 +3,6 @@ import s3client from "../../utils/s3Client"
 import { U } from "vitest/dist/reporters-BU_vXAUX.js";
 
 
-export const listBuckets = async () => {
-    console.log("listing buckets")
-    const command = new ListBucketsCommand({});
-
-    const { Buckets } = await s3client.send(command);
-
-    return Buckets;
-
-}
 
 type AddNewObjectToBucketProps = {
     bucketName: string,
@@ -19,54 +10,71 @@ type AddNewObjectToBucketProps = {
     content: string
 }
 
-export const addNewObjectToBucket = async ({ bucketName, objectName, content, }: AddNewObjectToBucketProps): Promise<PutObjectCommandOutput> => {
-    console.log("adding...")
-    const command = new PutObjectCommand({
-        Bucket: bucketName,
-        Key: objectName,
-        Body: content,
-    });
-    const response = await s3client.send(command);
 
-    return response;
-}
-
-export const listFilesInBucket = async ({ bucketName }): Promise<_Object[] | null> => {
-    console.log("listing files in bucket")
-    const command = new ListObjectsCommand({
-        Bucket: bucketName,
-    });
-
-    const response = await s3client.send(command);
-
-    const { Contents } = response;
-    if (!Contents) {
-        return null;
-    }
-
-    return Contents;
-}
 
 type GetFileFromBucketProps = {
     bucketName: string,
     objectName: string
 }
-export const getFileFromBucket = async ({ bucketName, objectName }: GetFileFromBucketProps): Promise<Uint8Array | null> => {
-    const command = new GetObjectCommand({
-        Bucket: bucketName,
-        Key: objectName,
-    });
 
-    const response = await s3client.send(command);
+export const s3Service = ({ bucketName }: { bucketName: string }) => {
+    return {
+        listBuckets: async () => {
+            console.log("listing buckets")
+            const command = new ListBucketsCommand({});
 
-    if (!response.Body) {
-        return null;
+            const { Buckets } = await s3client.send(command);
+
+            return Buckets;
+
+        },
+        addNewObjectToBucket: async ({ objectName, content, }: AddNewObjectToBucketProps): Promise<PutObjectCommandOutput> => {
+            console.log("adding...")
+            const command = new PutObjectCommand({
+                Bucket: bucketName,
+                Key: objectName,
+                Body: content,
+            });
+            const response = await s3client.send(command);
+
+            return response;
+        },
+        listFilesInBucket: async ({ bucketName }): Promise<_Object[] | null> => {
+            console.log("listing files in bucket")
+            const command = new ListObjectsCommand({
+                Bucket: bucketName,
+            });
+
+            const response = await s3client.send(command);
+
+            const { Contents } = response;
+            if (!Contents) {
+                return null;
+            }
+
+            return Contents;
+        },
+        getFileFromBucket: async ({ bucketName, objectName }: GetFileFromBucketProps): Promise<Uint8Array | null> => {
+            const command = new GetObjectCommand({
+                Bucket: bucketName,
+                Key: objectName,
+            });
+
+            const response = await s3client.send(command);
+
+            if (!response.Body) {
+                return null;
+            }
+
+
+            const buffer: Uint8Array = await response.Body.transformToByteArray();
+
+            return buffer;
+
+
+        }
     }
 
-
-    const buffer: Uint8Array = await response.Body.transformToByteArray();
-
-    return buffer;
-
-
 }
+
+
