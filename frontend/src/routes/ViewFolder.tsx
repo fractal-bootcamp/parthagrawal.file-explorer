de14a50e6f4a2a5f1ba86e647535a508783d2f09
Parth Agrawal <pvagrawal0@gmail.com>
2024-08-02 17:30:05 -0400

    name change

--- frontend/src/routes/ViewFolder.tsx
+++ frontend/src/routes/ViewFolder.tsx
@@ -46,7 +46,7 @@ export const ViewFolder = () => {
     return (
         <div className="flex flex-col h-screen w-screen items-center justify-center gap-3">
             <Window >
-                <WindowHeader> Folder Name </WindowHeader>
+                <WindowHeader> Folder on the Ground </WindowHeader>
                 <WindowContent>
                     <Table>
                         <TableHead>
